b0997e15dad0b63f3c3a39f55a4e3943be475f73
Adriana García <agglisette@gmail.com>
2021-11-17 14:40:03 -0600

    delete web app code

--- app.py
+++ app.py
@@ -6,31 +6,6 @@ import load_model
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route("/")
-def create_html():
-    return render_template("index.html")
-
-@app.route("/prediction", methods=['POST'])
-def info():
-    info = request.form
-    gender = info["gender"]
-    gradeID = info["gradeID"]
-    raisedHands = info["raisedHands"]
-    visitedResources = info["visitedResources"]
-    announcementsView = info["announcementsView"]
-    discussion = info["discussion"]
-    parentParticipating = info["parentParticipating"]
-    absenceDays = info["absenceDays"]
- 
-    prediction = load_model.createPrediction(gender, gradeID, raisedHands, visitedResources, announcementsView, discussion, parentParticipating, absenceDays)
-    if prediction == 0:
-        prediction_text = "Your student shows low achievement"
-    elif prediction == 1:
-        prediction_text = "Your student shows high achievement"
-    
-    return render_template("prediction.html", prediction_text = prediction_text)
-
-
 @app.route("/smart_aware_app", methods=['POST'])
 @cross_origin()
 def data():
